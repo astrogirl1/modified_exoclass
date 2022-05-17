00a8c1cdfc4b6c176478221ca2cb626b93b94b04
Meera D <meerand060@gmail.com>
2022-05-17 16:17:36 +0930

    adding_scripts_to_run_multiple_values

---
+++ run_17_05.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+import pandas as pd
+
+input_file = "/home/a1705053/Desktop/gamma.ini"
+
+# with open(input_file, "r") as file_in:
+file = pd.read_csv(input_file, names=["gamma", "decay"])
+# print(file)
+for i,data in file.iterrows():
+    os.system(f'sed -e "s:XXXGamma_dcdmXXX:{data.gamma}:g" -e "s:XXXdecayfractionXXX:{data.decay}:g" -e "s:XXXoutputXXX:output_{i}_:g" "example_dm_decay_template.ini" > example_dm_decay.ini')
+    subprocess.run(["./class","example_dm_decay.ini"])
